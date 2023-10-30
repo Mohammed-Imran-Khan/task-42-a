1eb4710f6b9e039591139bb7ba201f7340be8c55
Mohammed-Imran-Khan <127581145+Mohammed-Imran-Khan@users.noreply.github.com>
2023-10-30 16:13:52 +0530

    Update Contacts.js

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Contacts.css';
 
 const Contacts = () => {
   const [contacts, setContacts] = useState([]);
